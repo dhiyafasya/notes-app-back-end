9eebec8cdd7f1ac05e1ae1cbcdb4e3778a1ffb28
Dhiya Fasya <202210715272@mhs.ubharajaya.ac.id>
2025-05-24 07:34:26 +0700

    add src folder and update project

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const server = Hapi.server({    
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+        cors: {
+            origin: ['*'],
+        },
+    },
+});
